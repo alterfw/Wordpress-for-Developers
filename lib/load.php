77946c34a4e41f04ba5927d0e0b2640c8f7057bc
Sergio Vilar <sergiovilar.r@gmail.com>
2015-09-13 18:23:26 -0300

    Fix autoload for non WordPress environments

--- lib/load.php
+++ lib/load.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Base URL of lib
- * 
- * @var string
- */
-define('WD_BASE_URL', get_bloginfo('url') . '/wp-content/WD');
 
 /**
  * Autoloads classes
@@ -28,35 +22,50 @@ define('WD_BASE_URL', get_bloginfo('url') . '/wp-content/WD');
 		if(isset($file))
 			require_once dirname(__FILE__) . $ds . $file . '.php';
 	}
-	spl_autoload_register('wdAutoloader');
-
-/**
- * requires
- */	
-	// meta box functions
-	require_once dirname(__FILE__) . '/functions/meta_box.php';
-	
-	// taxonomy functions
-	require_once dirname(__FILE__) . '/functions/taxonomy.php';
-	
-	// pages functions
-	require_once dirname(__FILE__) . '/functions/pages.php';
-	
-	// wd form, is necessary include manually because the name do not follow the convention
-	require_once dirname(__FILE__) . '/Form/WD_Form.php';
 
 /**
  * Loads styles and scripts
  */
-	function wd_admin_init(){
-		$path = WD_BASE_URL . '/public';
-		
-		// css
-		wp_register_style('wd', $path . '/css/style.css', false);
-		wp_enqueue_style('wd');
+function wd_admin_init(){
+  $path = WD_BASE_URL . '/public';
 
-		// js
-		wp_register_script('wd', $path . '/js/load.js', false);
-		wp_enqueue_script('wd');
-	}
-	add_action('admin_init', 'wd_admin_init');
+  // css
+  wp_register_style('wd', $path . '/css/style.css', false);
+  wp_enqueue_style('wd');
+
+  // js
+  wp_register_script('wd', $path . '/js/load.js', false);
+  wp_enqueue_script('wd');
+}
+
+function do_load() {
+
+  /**
+   * Base URL of lib
+   *
+   * @var string
+   */
+  define('WD_BASE_URL', get_bloginfo('url') . '/wp-content/WD');
+
+  /**
+   * requires
+   */
+  // meta box functions
+  require_once dirname(__FILE__) . '/functions/meta_box.php';
+
+  // taxonomy functions
+  require_once dirname(__FILE__) . '/functions/taxonomy.php';
+
+  // pages functions
+  require_once dirname(__FILE__) . '/functions/pages.php';
+
+  // wd form, is necessary include manually because the name do not follow the convention
+  require_once dirname(__FILE__) . '/Form/WD_Form.php';
+
+
+  add_action('admin_init', 'wd_admin_init');
+
+}
+
+if(function_exists('get_bloginfo')) do_load(); // Load only in a WordPress environment
+spl_autoload_register('wdAutoloader');
